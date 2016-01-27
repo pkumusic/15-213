d1f6158f58276491cc3bd6fe6c8524deb348439e
Yuezhang Li <yuezhanl@lemonshark.ics.cs.cmu.edu>
2016-01-26 22:40:36 -0500

    first commit

---
+++ bits.h
@@ -0,0 +1,31 @@
+//1
+int thirdBits();
+int test_thirdBits();
+int isTmin(int);
+int test_isTmin(int);
+//2
+int isNotEqual(int, int);
+int test_isNotEqual(int, int);
+int anyOddBit();
+int test_anyOddBit();
+int negate(int);
+int test_negate(int);
+//3
+int conditional(int, int, int);
+int test_conditional(int, int, int);
+int subOK(int, int);
+int test_subOK(int, int);
+int isGreater(int, int);
+int test_isGreater(int, int);
+//4
+int bitParity(int);
+int test_bitParity(int);
+int howManyBits(int);
+int test_howManyBits(int);
+//float
+unsigned float_half(unsigned);
+unsigned test_float_half(unsigned);
+unsigned float_i2f(int);
+unsigned test_float_i2f(int);
+int float_f2i(unsigned);
+int test_float_f2i(unsigned);
---
+++ decl.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define TMin INT_MIN
+#define TMax INT_MAX
+
+#include "btest.h"
+#include "bits.h"
+
+test_rec test_set[] = {
+//1
+ {"thirdBits", (funct_t) thirdBits, (funct_t) test_thirdBits, 0,
+    "! ~ & ^ | + << >>", 8, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"isTmin", (funct_t) isTmin, (funct_t) test_isTmin, 1, "! ~ & ^ | +", 10, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+//2
+ {"isNotEqual", (funct_t) isNotEqual, (funct_t) test_isNotEqual, 2,
+    "! ~ & ^ | + << >>", 6, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"anyOddBit", (funct_t) anyOddBit, (funct_t) test_anyOddBit, 1,
+    "! ~ & ^ | + << >>", 12, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"negate", (funct_t) negate, (funct_t) test_negate, 1,
+    "! ~ & ^ | + << >>", 5, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+//3
+ {"conditional", (funct_t) conditional, (funct_t) test_conditional, 3, "! ~ & ^ | << >>", 16, 3,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"subOK", (funct_t) subOK, (funct_t) test_subOK, 2,
+    "! ~ & ^ | + << >>", 20, 3,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"isGreater", (funct_t) isGreater, (funct_t) test_isGreater, 2,
+    "! ~ & ^ | + << >>", 24, 3,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+//4
+ {"bitParity", (funct_t) bitParity, (funct_t) test_bitParity, 1, "! ~ & ^ | + << >>", 20, 4,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"howManyBits", (funct_t) howManyBits, (funct_t) test_howManyBits, 1, "! ~ & ^ | + << >>", 90, 4,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+//float
+ {"float_half", (funct_t) float_half, (funct_t) test_float_half, 1,
+    "$", 30, 4,
+     {{1, 1},{1,1},{1,1}}},
+ {"float_i2f", (funct_t) float_i2f, (funct_t) test_float_i2f, 1,
+    "$", 30, 4,
+     {{1, 1},{1,1},{1,1}}},
+ {"float_f2i", (funct_t) float_f2i, (funct_t) test_float_f2i, 1,
+    "$", 30, 4,
+     {{1, 1},{1,1},{1,1}}},
+  {"", NULL, NULL, 0, "", 0, 0,
+   {{0, 0},{0,0},{0,0}}}
+};
